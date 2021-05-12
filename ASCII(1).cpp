2c07c5ea399eb43d9ead59dcb4838bc9fcafb8c1
BHARGAVI.V <74961558+Bhar-02@users.noreply.github.com>
2021-05-12 21:55:20 +0530

    Pgm to find ASCII value

--- ASCII(1).cpp
+++ ASCII(1).cpp
@@ -2,10 +2,10 @@
 using namespace std ;
 int main()
 {
-    char x;
+    char x; // reads the input from the user
     cout<<"Enter the character :";
     cin>>x;
-    cout<<"ASCII Value :"<<int(x);
+    cout<<"ASCII Value :"<<int(x); // prints the value
     return 0;
 
 }
